b7009838a17922d65d97b74acbed6df5b7a69f50
Jovan <jovanzaric98@hotmail.com>
2024-01-25 19:21:48 +0100

    Updated Code

--- DataConverter.py
+++ DataConverter.py
@@ -2,6 +2,7 @@ import PySimpleGUI as sg
 from pathlib import Path
 import pandas as pd
 import csv
+from lxml import etree
 
 # OUTPUT FILES xml,csv,json,xlsx,md,html
 # INPUT FILES csv,xlsx,xml,son
@@ -34,68 +35,107 @@ CONVERSION_FUNCTIONS = {
     ("xlsx", "md"): (pd.read_excel, pd.DataFrame.to_markdown)
 }
 
+# Define a function to remove whitespace from a file
+def remove_whitespace_from_file(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    content_without_whitespace = content.replace(" ", "")
+    with open(file_path, 'w') as file:
+        file.write(content_without_whitespace)
+
 # Generic conversion function
 def convert_files(input_path, output_path, input_ext, output_ext):
-    
     try:
         read_func, write_func = CONVERSION_FUNCTIONS.get((input_ext, output_ext), (None, None))
         
         if read_func is None or write_func is None:
-            window["-OUTPUT_WINDOW-"].update(">>> Unsupported conversion!",text_color="#ff4545")
-            
+            window["-OUTPUT_WINDOW-"].update("Unsupported conversion!", text_color="#ff4545")
+            return
+
+        # Check and remove whitespace from input file
+        if ' ' in open(input_path, 'r').read():
+            remove_whitespace_from_file(input_path)
+
         df = read_func(input_path)
-        write_func(df, output_path)
-        window["-OUTPUT_WINDOW-"].update(f"Successfully converted {Path(input_path).stem} {input_ext.upper()} to {Path(output_path).stem} {output_ext.upper()}",text_color="#51e98b")
         
+        # Check if the output extension is XML and remove whitespaces
+        if output_ext.upper() == 'XML':
+            output_path_temp = output_path.replace(".xml", "_temp.xml")
+            write_func(df, output_path_temp)
+            remove_whitespace_from_file(output_path_temp)
+            window["-OUTPUT_WINDOW-"].update(f"Successfully converted {Path(input_path).stem} {input_ext.upper()} to {Path(output_path).stem} {output_ext.upper()} (INFO: {input_ext.upper()} contained whitespaces, which were removed for XML Conversion!)", text_color="#51e98b")
+        else:
+            write_func(df, output_path)
+            window["-OUTPUT_WINDOW-"].update(f"Successfully converted {Path(input_path).stem} {input_ext.upper()} to {Path(output_path).stem} {output_ext.upper()}", text_color="#51e98b")
+
     except FileNotFoundError:
-        window["-OUTPUT_WINDOW-"].update(f">>> {input_ext.upper()} File not found!",text_color="#ff4545")
+        window["-OUTPUT_WINDOW-"].update(f"{input_ext.upper()} File not found!", text_color="#ff4545")
+    except Exception as e:
+        window["-OUTPUT_WINDOW-"].update(f"ERROR: {e}", text_color="#ff4545")
         
-# Function to read input files with Pandas
 def read_file_data(file):
     try:
         file_suffix_in_input = Path(file).suffix.upper().strip(".")
         df = None
         
         if file_suffix_in_input == "CSV":
+            # Use the Sniffer class to automatically detect the delimiter
             with open(file, 'r', newline='') as csv_file:
-                
-                # Use the Sniffer class to automatically detect the delimiter
                 sniffer = csv.Sniffer()
                 sample_data = csv_file.read(512)  # Read a sample of the file to determine the delimiter
-                
-                # Determine the CSV Delimiter
                 dialect = sniffer.sniff(sample_data)
-                separator = dialect.delimiter
-                print(f"Seperator: {separator}")
-                
+                csv_delimiter = dialect.delimiter
+
+                if csv_delimiter == ";":
+                    # Read the content with the old delimiter
+                    with open(file, 'r', newline='') as csv_file_read:
+                        reader = csv.reader(csv_file_read, delimiter=";")
+                        rows = list(reader)
+
+                    # Write the content with the new delimiter
+                    with open(file, 'w', newline='') as csv_file_write:
+                        writer = csv.writer(csv_file_write, delimiter=",")
+                        writer.writerows(rows)
+
+                    print("Delimiter successfully changed!")
+
+            # Reopen the file after changing the delimiter
+            with open(file, 'r', newline='') as csv_file:
                 # Determine the CSV line ending
-                line_endings = {'\r\n': 'Windows', '\n': 'Unix/Linux', '\r': 'Old Mac'}
+                line_endings = {"\r\n": "Windows", "\n": "Unix/Linux", "\r": "Old Mac"}
                 detected_line_ending = line_endings.get(csv_file.newlines, 'Unknown')
-                print(f"Detected Line Ending: {detected_line_ending}")
-            
-        if file_suffix_in_input == "CSV":
-            df = pd.read_csv(file, sep=separator, lineterminator="\r", index_col=False)
-            window["-OUTPUT_WINDOW-"].update(df,text_color="white")
+
+                if detected_line_ending == "Windows":
+                    lineterminator = "\r\n"
+                elif detected_line_ending == "Unix/Linux":
+                    lineterminator = "\n"
+                elif detected_line_ending == "Old Mac":
+                    lineterminator = "\r"
+                else:
+                    lineterminator = None
+
+                df = pd.read_csv(csv_file, delimiter=",", lineterminator=lineterminator, index_col=0)
+                
         elif file_suffix_in_input == "XML":
             df = pd.read_xml(file)
-            window["-OUTPUT_WINDOW-"].update(df,text_color="white")
         elif file_suffix_in_input == "JSON":
             df = pd.read_json(file)
-            window["-OUTPUT_WINDOW-"].update(df,text_color="white")
         elif file_suffix_in_input == "XLSX":
             df = pd.read_excel(file)
-            window["-OUTPUT_WINDOW-"].update(df,text_color="white")
         elif file_suffix_in_input == "":
-            window["-OUTPUT_WINDOW-"].update(">>> Error Input is empty, cannot read nothing!",text_color="#ff4545")
+            raise ValueError("Error: Input is empty. Cannot read nothing!")
+        
         if df is not None:
             column_names = df.columns.tolist()
             window["-COLUMNS-"].update(values=column_names)
+            window["-OUTPUT_WINDOW-"].update(df, text_color="white")
             return df, column_names
         else:
             return None, []
-        
+
     except Exception as e:
-        window["-OUTPUT_WINDOW-"].update(f"ERROR: {e}",text_color="#ff4545")
+        window["-OUTPUT_WINDOW-"].update(f"ERROR: {e}", text_color="#ff4545")
+        return None, []
 
 # ====== Graphical User Interface settings ====== #
 
@@ -132,7 +172,7 @@ layout_inputs = [[sg.Text("Select an input file for conversion:")],
 
 layout_data_properties = [[sg.Text("Columns:"),sg.Combo(values="",key="-COLUMNS-",size=(10, 1)),sg.Text("To get the Column names, read a file first.")]]
 
-layout_output_and_exit = [[sg.Multiline(size=(54, 12), key="-OUTPUT_WINDOW-")],
+layout_output_and_exit = [[sg.Multiline(size=(62, 12), key="-OUTPUT_WINDOW-")],
                              [sg.Button("Exit", expand_x=True)]]
 
 layout_checkbox = [[sg.Checkbox(text="Show Data Properties",default=False,key="-CHECKBOX_DATA_PROPERTIES-",enable_events=True),sg.Checkbox(text="Show Output Window",default=False,key="-CHECKBOX_SHOW_OUTPUT-",enable_events=True)],
